8f8acb777035473bcdafb21d1461083f8231fce9
Kawe Maximo <kawemaximogodoi@hotmail.com>
2023-04-21 18:32:52 -0300

    Sex. 21 de Abr

--- Pages/src/Home/index.php
+++ Pages/src/Home/index.php
@@ -24,24 +24,24 @@ $nome = $_SESSION['nome'];
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="../../Assets/img/favicon.png" rel="icon">
-  <link href="../../Assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="../../assets/img/favicon.png" rel="icon">
+  <link href="../../assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.gstatic.com" rel="preconnect">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="../../Assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="../../Assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="../../Assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="../../Assets/vendor/quill/quill.snow.css" rel="stylesheet">
-  <link href="../../Assets/vendor/quill/quill.bubble.css" rel="stylesheet">
-  <link href="../../Assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="../../Assets/vendor/simple-datatables/style.css" rel="stylesheet">
+  <link href="../../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="../../assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="../../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="../../assets/vendor/quill/quill.snow.css" rel="stylesheet">
+  <link href="../../assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+  <link href="../../assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="../../assets/vendor/simple-datatables/style.css" rel="stylesheet">
 
   <!-- Template Main CSS File -->
-  <link href="../../Assets/css/style.css" rel="stylesheet">
+  <link href="../../assets/css/style.css" rel="stylesheet">
 
 </head>
 
@@ -52,7 +52,7 @@ $nome = $_SESSION['nome'];
 
     <div class="d-flex align-items-center justify-content-between">
       <a href="index.html" class="logo d-flex align-items-center">
-        <img src="../../Assets/img/logo.png" alt="">
+        <img src="../../assets/img/logo.png" alt="">
         <span class="d-none d-lg-block">MaxG2</span>
       </a>
       <i class="bi bi-list toggle-sidebar-btn"></i>
@@ -167,7 +167,7 @@ $nome = $_SESSION['nome'];
 
             <li class="message-item">
               <a href="#">
-                <img src="../../Assets/img/messages-1.jpg" alt="" class="rounded-circle">
+                <img src="../../assets/img/messages-1.jpg" alt="" class="rounded-circle">
                 <div>
                   <h4>Maria Hudson</h4>
                   <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
@@ -181,7 +181,7 @@ $nome = $_SESSION['nome'];
 
             <li class="message-item">
               <a href="#">
-                <img src="../../Assets/img/messages-2.jpg" alt="" class="rounded-circle">
+                <img src="../../assets/img/messages-2.jpg" alt="" class="rounded-circle">
                 <div>
                   <h4>Anna Nelson</h4>
                   <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
@@ -195,7 +195,7 @@ $nome = $_SESSION['nome'];
 
             <li class="message-item">
               <a href="#">
-                <img src="../../Assets/img/messages-3.jpg" alt="" class="rounded-circle">
+                <img src="../../assets/img/messages-3.jpg" alt="" class="rounded-circle">
                 <div>
                   <h4>David Muldon</h4>
                   <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
@@ -221,7 +221,7 @@ $nome = $_SESSION['nome'];
         ?>
 
           <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-            <img src="../../Assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
+            <img src="../../assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
             <span class="d-none d-md-block dropdown-toggle ps-2"><?php echo $nome ?></span>
           </a><!-- End Profile Iamge Icon -->
 
@@ -830,35 +830,35 @@ $nome = $_SESSION['nome'];
                     </thead>
                     <tbody>
                       <tr>
-                        <th scope="row"><a href="#"><img src="../../Assets/img/product-1.jpg" alt=""></a></th>
+                        <th scope="row"><a href="#"><img src="../../assets/img/product-1.jpg" alt=""></a></th>
                         <td><a href="#" class="text-primary fw-bold">Ut inventore ipsa voluptas nulla</a></td>
                         <td>$64</td>
                         <td class="fw-bold">124</td>
                         <td>$5,828</td>
                       </tr>
                       <tr>
-                        <th scope="row"><a href="#"><img src="../../Assets/img/product-2.jpg" alt=""></a></th>
+                        <th scope="row"><a href="#"><img src="../../assets/img/product-2.jpg" alt=""></a></th>
                         <td><a href="#" class="text-primary fw-bold">Exercitationem similique doloremque</a></td>
                         <td>$46</td>
                         <td class="fw-bold">98</td>
                         <td>$4,508</td>
                       </tr>
                       <tr>
-                        <th scope="row"><a href="#"><img src="../../Assets/img/product-3.jpg" alt=""></a></th>
+                        <th scope="row"><a href="#"><img src="../../assets/img/product-3.jpg" alt=""></a></th>
                         <td><a href="#" class="text-primary fw-bold">Doloribus nisi exercitationem</a></td>
                         <td>$59</td>
                         <td class="fw-bold">74</td>
                         <td>$4,366</td>
                       </tr>
                       <tr>
-                        <th scope="row"><a href="#"><img src="../../Assets/img/product-4.jpg" alt=""></a></th>
+                        <th scope="row"><a href="#"><img src="../../assets/img/product-4.jpg" alt=""></a></th>
                         <td><a href="#" class="text-primary fw-bold">Officiis quaerat sint rerum error</a></td>
                         <td>$32</td>
                         <td class="fw-bold">63</td>
                         <td>$2,016</td>
                       </tr>
                       <tr>
-                        <th scope="row"><a href="#"><img src="../../Assets/img/product-5.jpg" alt=""></a></th>
+                        <th scope="row"><a href="#"><img src="../../assets/img/product-5.jpg" alt=""></a></th>
                         <td><a href="#" class="text-primary fw-bold">Sit unde debitis delectus repellendus</a></td>
                         <td>$79</td>
                         <td class="fw-bold">41</td>
@@ -1123,31 +1123,31 @@ $nome = $_SESSION['nome'];
 
               <div class="news">
                 <div class="post-item clearfix">
-                  <img src="../../Assets/img/news-1.jpg" alt="">
+                  <img src="../../assets/img/news-1.jpg" alt="">
                   <h4><a href="#">Nihil blanditiis at in nihil autem</a></h4>
                   <p>Sit recusandae non aspernatur laboriosam. Quia enim eligendi sed ut harum...</p>
                 </div>
 
                 <div class="post-item clearfix">
-                  <img src="../../Assets/img/news-2.jpg" alt="">
+                  <img src="../../assets/img/news-2.jpg" alt="">
                   <h4><a href="#">Quidem autem et impedit</a></h4>
                   <p>Illo nemo neque maiores vitae officiis cum eum turos elan dries werona nande...</p>
                 </div>
 
                 <div class="post-item clearfix">
-                  <img src="../../Assets/img/news-3.jpg" alt="">
+                  <img src="../../assets/img/news-3.jpg" alt="">
                   <h4><a href="#">Id quia et et ut maxime similique occaecati ut</a></h4>
                   <p>Fugiat voluptas vero eaque accusantium eos. Consequuntur sed ipsam et totam...</p>
                 </div>
 
                 <div class="post-item clearfix">
-                  <img src="../../Assets/img/news-4.jpg" alt="">
+                  <img src="../../assets/img/news-4.jpg" alt="">
                   <h4><a href="#">Laborum corporis quo dara net para</a></h4>
                   <p>Qui enim quia optio. Eligendi aut asperiores enim repellendusvel rerum cuder...</p>
                 </div>
 
                 <div class="post-item clearfix">
-                  <img src="../../Assets/img/news-5.jpg" alt="">
+                  <img src="../../assets/img/news-5.jpg" alt="">
                   <h4><a href="#">Et dolores corrupti quae illo quod dolor</a></h4>
                   <p>Odit ut eveniet modi reiciendis. Atque cupiditate libero beatae dignissimos eius...</p>
                 </div>
@@ -1167,17 +1167,17 @@ $nome = $_SESSION['nome'];
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
-  <script src="../../Assets/vendor/apexcharts/apexcharts.min.js"></script>
-  <script src="../../Assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="../../Assets/vendor/chart.js/chart.umd.js"></script>
-  <script src="../../Assets/vendor/echarts/echarts.min.js"></script>
-  <script src="../../Assets/vendor/quill/quill.min.js"></script>
-  <script src="../../Assets/vendor/simple-datatables/simple-datatables.js"></script>
-  <script src="../../Assets/vendor/tinymce/tinymce.min.js"></script>
-  <script src="../../Assets/vendor/php-email-form/validate.js"></script>
+  <script src="../../assets/vendor/apexcharts/apexcharts.min.js"></script>
+  <script src="../../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="../../assets/vendor/chart.js/chart.umd.js"></script>
+  <script src="../../assets/vendor/echarts/echarts.min.js"></script>
+  <script src="../../assets/vendor/quill/quill.min.js"></script>
+  <script src="../../assets/vendor/simple-datatables/simple-datatables.js"></script>
+  <script src="../../assets/vendor/tinymce/tinymce.min.js"></script>
+  <script src="../../assets/vendor/php-email-form/validate.js"></script>
 
   <!-- Template Main JS File -->
-  <script src="../../Assets/js/main.js"></script>
+  <script src="../../assets/js/main.js"></script>
 
 </body>
 
